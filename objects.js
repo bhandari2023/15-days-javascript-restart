c593adcba27bc6f43c394bb359a3931944bfd04c
bhandari2023 <152293382+bhandari2023@users.noreply.github.com>
2026-04-13 05:05:46 +0000

    objects

---
+++ objects.js
@@ -0,0 +1,17 @@
+//singleton
+ //object litrals
+ 
+ const JsUser ={
+    name: "pankaj",
+    age: "38",
+    location: "haldwani",
+    email:"pankajbhandari2010@gmail.com",
+    isLoggedin: false,
+    lastLoginDays:["monday","saturday"]
+
+ }
+
+ console.log(JsUser.email);
+ console.log(JsUser["name"]);
+ 
+
